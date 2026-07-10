36a1cecad560701a8aa22baf4d8364d6842f898d
cavetthubbis72-png <cavetthubbis72@gmail.com>
2026-07-10 14:33:15 +0800

    Create HomeostaticSeasonFix.java

---
+++ src/main/java/com/yourname/homeostaticfix/HomeostaticSeasonFix.java
@@ -0,0 +1,13 @@
+package com.yourname.homeostaticfix;
+
+import net.neoforged.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("homeostaticfix")
+public class HomeostaticSeasonFix {
+    public static final Logger LOGGER = LogManager.getLogger("HomeostaticFix");
+    public HomeostaticSeasonFix() {
+        LOGGER.info("Homeostatic Season Fix loaded successfully!");
+    }
+}
